5509485cc9e288f205d91a94d23ddaa7e6eed2b6
jasminumbi <jasminumbanhowar@gmail.com>
2024-08-12 20:24:33 -0500

    studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,28 @@
 //TODO: Add Your Code Below
+
+//need to figure out how to loop through the rest of the astronauts
+window.addEventListener("load", function() {
+
+    const container = document.getElementById('container');
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+      response.json().then(function (json) {
+        for (let i = 0; i < json.length; i++);
+        let astro = json[i];
+        container.innerHTML += `
+        <div class="astronaut"> 
+          <div class="bio">
+            <h3>${astro.firstName} ${astro.lastName}</h3>
+            <ul>
+              <li>Hours in space: ${astro.hoursInSpace}</li>
+              <li>Active: ${astro.active}</li>
+              <li>Skills: ${astro.skills}</li>
+              </ul>
+            </div>
+            <img class="avatar" src="${astro.picture}"
+        </div>
+        `;
+      })
+    }
+  )
+  })
